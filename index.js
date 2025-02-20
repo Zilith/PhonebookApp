213432569ce4dcc642a37ebd53243c63adf8122e
Diego Zapata <99824449+Zilith@users.noreply.github.com>
2025-02-20 18:10:31 -0500

    feat(index.js): implement ex. 3.4, update README

--- index.js
+++ index.js
@@ -60,6 +60,12 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
