0f0fa0cb92b71736c65835b2ddc6fbc7c51d1881
Diego Zapata <99824449+Zilith@users.noreply.github.com>
2025-02-28 15:22:55 -0500

    uibuild

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
 
+app.use(express.static("dist"))
 app.use(cors());
-
 app.use(express.json());
 morgan.token("data", function (req, res) {
   return JSON.stringify(req.body);
