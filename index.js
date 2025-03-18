fa31c726907bac672701ceb0123047e1ac4be508
Diego Zapata <99824449+Zilith@users.noreply.github.com>
2025-03-18 14:13:14 -0500

    feat(index.js): handle ValidationError with middleware

--- index.js
+++ index.js
@@ -70,9 +70,12 @@ app.post("/api/persons", (req, res, next) => {
           phone: body.phone,
         });
 
-        person.save().then((savedPerson) => {
-          res.json(savedPerson);
-        });
+        person
+          .save()
+          .then((savedPerson) => {
+            res.json(savedPerson);
+          })
+          .catch((error) => next(error));
       }
     })
     .catch((error) => next(error));
@@ -117,6 +120,8 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === "CastError") {
     return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).send({ error: error.message });
   }
 };
 
