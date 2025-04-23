666ec6624b8e525e3f60e56aa93926ec7ddf4c0d
Diego Zapata <99824449+Zilith@users.noreply.github.com>
2025-04-23 13:42:09 -0500

    feat(models/person.js): remove MongoDB connection initialization from model

--- models/person.js
+++ models/person.js
@@ -1,16 +1,4 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-
-const url = process.env.MONGODB_URI;
-
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("Something gone wrong connecting to MongoDB", error);
-  });
+const mongoose = require('mongoose')
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -24,18 +12,18 @@ const personSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator: function (v) {
-        return /\d{2,3}-\d{8}/.test(v);
+        return /\d{2,3}-\d{8}/.test(v)
       },
     },
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__V;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__V
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
